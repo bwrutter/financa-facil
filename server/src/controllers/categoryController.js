47cb84f087fcfc045f804e12f9bdaf1d0fee7a11
bwrutter <bryanwrutter96@gmail.com>
2025-05-02 23:24:05 -0300

    feat: criado crud de categorias

--- server/src/controllers/categoryController.js
+++ server/src/controllers/categoryController.js
@@ -21,4 +21,56 @@ const getCategory = async (req, res) => {
   }
 };
 
-export { createCategory, getCategory };
+const getCategoryById = async (req, res) => {
+  try {
+    const categories = await Category.findById(req.params.id);
+
+    if (!categories) {
+      return res.status(404).json({ error: "categories not found" });
+    }
+
+    res.json(categories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const updateCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, userId } = req.body;
+
+    const updatedCategory = await Category.findByIdAndUpdate(id, {
+      name,
+      userId,
+    });
+
+    if (!updatedCategory) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    res.json({ message: "Category updated successfully", updatedCategory });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Category.findByIdAndDelete(id);
+    res.json({ message: "Category deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export {
+  createCategory,
+  getCategory,
+  getCategoryById,
+  deleteCategory,
+  updateCategory,
+};
--- server/src/routes/categoryRoutes.js
+++ server/src/routes/categoryRoutes.js
@@ -2,11 +2,17 @@ import express from "express";
 import {
   createCategory,
   getCategory,
+  getCategoryById,
+  deleteCategory,
+  updateCategory,
 } from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 router.post("/", createCategory);
 router.get("/", getCategory);
+router.get("/:id", getCategoryById);
+router.delete("/:id", deleteCategory);
+router.put("/:id", updateCategory);
 
 export default router;

d90cfd8a471e5d644e18b30efe77bcad19f20c05
bwrutter <bryanwrutter96@gmail.com>
2025-05-17 15:58:25 -0300

    chore: ajustado deploy do front

--- app/src/firebase/config.js
+++ app/src/firebase/config.js
@@ -1,8 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-console.log('Firebase App:', initializeApp);
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,

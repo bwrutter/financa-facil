66a28a8931d15ef48b9d877168b81e907d4d9062
bwrutter <bryanwrutter96@gmail.com>
2025-05-17 18:55:28 -0300

    chore: ajuste deploy

--- server/src/config/firebase.js
+++ server/src/config/firebase.js
@@ -1,7 +1,12 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccountJson = Buffer.from(
+  process.env.FIREBASE_SERVICE_ACCOUNT_BASE64,
+  'base64'
+).toString('utf-8');
+
+const serviceAccount = JSON.parse(serviceAccountJson);
 
 initializeApp({
   credential: cert(serviceAccount),

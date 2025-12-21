4dcc3bba13976db6218d1e4b472d99b16b131d21
bwrutter <bryanwrutter96@gmail.com>
2025-12-21 20:59:33 -0300

    feat: adicionado estrutura das docs

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ import express from "express";
 import authRoutes from "./modules/auth/auth.router.js";
 import accountRoutes from "./modules/accounts/account.routes.js";
 import transactionRoutes from "./modules/transactions/transaction.routes.js";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import yaml from "js-yaml";
+
+const swaggerDocument = yaml.load(fs.readFileSync("./src/docs/swagger.yml", "utf8"));
 
 const app = express();
 
 app.use(express.json())
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/auth", authRoutes);
 app.use("/accounts", accountRoutes);
 app.use("/transactions", transactionRoutes);
